39d29f8a327881f1c23a8265bb44585a0c66166a
kobasrb <anthonykobanovic@icloud.com>
2023-12-15 11:30:07 +0100

    Premier commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'xomFjQyBT53w8aEE', // TODO
+    secretAccessKey: 'K0Nh4cUe5Sgf0C2GgjQnrKxDP4HY4vgf8gOb9tg2', // TODO
+    endpoint: 'esgi-cloud-exam-bucket-07074', // TODO
+    bucket: 's3.tebi.io', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
